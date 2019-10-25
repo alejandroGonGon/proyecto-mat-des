d813d0e4af8d31b56b488302eb1d2fc635639723
alejandrogon1418 <56725406+alejandrogon1418@users.noreply.github.com>
2019-10-25 13:20:54 -0300

    First commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,89 @@
+let toggleMenu = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
+
+let toggleInicio = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
+
+let toggleProyecto = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+  
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
+let toggleObjetivos = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
+
+let toggleRecoleccion = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+  
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
+
+let toggleResultados = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+  
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
+
+let toggleConcluciones = () => {
+  let menuContainer = document.querySelector("#menu-container")
+
+
+  
+  if (menuContainer.style.display !== 'block') {
+
+    menuContainer.style.display = 'block'
+  } else {
+    menuContainer.style.display = 'none'
+  }
+}
